96af425d5ff0d425c8076a46b423e2bc3c9e36e1
Sean Wong <seanwong24@live.com>
2022-11-03 19:16:22 -0700

    update mesh.py to use the general db generation script

--- mesh.py
+++ mesh.py
@@ -1,10 +1,10 @@
+# %% imports
+from typing import Sequence
 import shapefile
 import netCDF4 as nc
-import sqlite3
-import json
-import os
+from gwfvis_db import Dimension, Location, Options, Value, Variable, generate_gwfvis_db, Info
 
-# region configs
+# %% configs
 # TODO modify the configs if needed
 db_path = 'output/mesh.gwfvisdb'
 nc_db_file_path = 'data/mesh/Drainage_database/BowBanff_MESH_drainage_database.nc'
@@ -13,174 +13,121 @@ shape_file_path = 'data/mesh/Shape/bow_distributed.shp'
 layers = [1, 2, 3]
 nc_file_base_path = 'data/mesh/MESH_state'
 nc_file_path_and_variable_name_pairs = [
-    [f'{nc_file_base_path}/STGW_M_GRD.nc', 'STGW', 'Total water storage [mm]', 'mm', None],
-    [f'{nc_file_base_path}/SNO_M_GRD.nc', 'SNO', 'Liquid water content of the snow [mm]', 'mm', None],
-    [f'{nc_file_base_path}/LQWSSNO_M_GRD.nc', 'LQWSSNO', 'Liquid water content of the snow [mm]', 'mm', None],
-    [f'{nc_file_base_path}/LQWSPND_M_GRD.nc', 'LQWSPND', 'Liquid water storage of ponded water [mm]', 'mm', None],
-    [f'{nc_file_base_path}/LQWSSOL_M_IG$$layer$$_GRD.nc', 'LQWSSOL', 'Liquid water storage in the soil [mm]', 'mm', layers],
-    [f'{nc_file_base_path}/FZWSSOL_M_IG$$layer$$_GRD.nc', 'FZWSSOL', 'Frozen water storage in the soil [mm]', 'mm', layers],
-    [f'{nc_file_base_path}/LQWSCAN_M_GRD.nc', 'LQWSCAN', 'Liquid water interception in the canopy [mm]', 'mm', None],
-    [f'{nc_file_base_path}/FZWSCAN_M_GRD.nc', 'FZWSCAN', 'Frozen water interception in the canopy [mm]', 'mm', None],
-    [f'{nc_file_base_path}/STGGW_M_GRD.nc', 'STGGW', 'Groundwater zone storage [mm]', 'mm', None],
-    [f'{nc_file_base_path}/RFF_M_GRD.nc', 'RFF', 'Total runoff [mm]', 'mm', None],
+    [f'{nc_file_base_path}/STGW_M_GRD.nc', 'STGW',
+        'Total water storage [mm]', 'mm', False],
+    [f'{nc_file_base_path}/SNO_M_GRD.nc', 'SNO',
+        'Liquid water content of the snow [mm]', 'mm', False],
+    [f'{nc_file_base_path}/LQWSSNO_M_GRD.nc', 'LQWSSNO',
+        'Liquid water content of the snow [mm]', 'mm', False],
+    [f'{nc_file_base_path}/LQWSPND_M_GRD.nc', 'LQWSPND',
+        'Liquid water storage of ponded water [mm]', 'mm', False],
+    [f'{nc_file_base_path}/LQWSSOL_M_IG$$layer$$_GRD.nc', 'LQWSSOL',
+        'Liquid water storage in the soil [mm]', 'mm', True],
+    [f'{nc_file_base_path}/FZWSSOL_M_IG$$layer$$_GRD.nc', 'FZWSSOL',
+        'Frozen water storage in the soil [mm]', 'mm', True],
+    [f'{nc_file_base_path}/LQWSCAN_M_GRD.nc', 'LQWSCAN',
+        'Liquid water interception in the canopy [mm]', 'mm', False],
+    [f'{nc_file_base_path}/FZWSCAN_M_GRD.nc', 'FZWSCAN',
+        'Frozen water interception in the canopy [mm]', 'mm', False],
+    [f'{nc_file_base_path}/STGGW_M_GRD.nc', 'STGGW',
+        'Groundwater zone storage [mm]', 'mm', False],
+    [f'{nc_file_base_path}/RFF_M_GRD.nc',
+        'RFF', 'Total runoff [mm]', 'mm', False],
 ]
-# endregion
 
-# region getting ready
-try:
-    os.remove(db_path)
-except:
-    pass
-
-db_connection = sqlite3.connect(db_path)
-db_cursor = db_connection.cursor()
-
+# %% getting ready
 nc_file_path = nc_file_path_and_variable_name_pairs[0][0]
 
 reader = shapefile.Reader(shape_file_path)
 nc_db = nc.Dataset(nc_db_file_path)
 dataset = nc.Dataset(nc_file_path)
-# endregion
 
-# region create info table
-db_cursor.execute('''
-    create table info (
-        key varchar primary key,
-        value text,
-        label varchar
-    )
-''')
-db_cursor.execute('''
-    insert into info (key, value, label) values ('name', 'mesh', 'Name')
-''')
-db_cursor.execute('''
-    insert into info (key, value, label) values ('description', 'something...', 'Description')
-''')
-# endregion
+# %% info
+info = [
+    Info(key='name', value='mesh', label='Name'),
+    Info(key='description', value='something...', label='Description')
+]
 
-# region create location table
-db_cursor.execute('''
-    create table location (
-        id integer primary key,
-        geometry text,
-        metadata text
-    )
-''')
-
+# %% locations
 ids = list(nc_db.variables['seg_id'][:])
-location_ids = []
+locations: Sequence[Location] = []
 for shape_record in reader.shapeRecords():
     id = int(shape_record.record['COMID'])
-    location_ids.append(id)
     geometry = shape_record.shape.__geo_interface__
-    geometry_json = json.dumps(geometry, indent=2)
     fields = reader.fields[1:]
     field_names = [field[0] for field in fields]
     metadata = dict(zip(field_names, shape_record.record))
-    metadata_json = json.dumps(metadata, indent=2)
-    db_cursor.execute('''
-        insert into location (id, geometry, metadata) values (?, ?, ?)
-    ''', [id, geometry_json, metadata_json])
-# endregion
-
-# region create dimension table
-db_cursor.execute('''
-    create table dimension (
-        id integer primary key,
-        name varchar not null,
-        size int not null,
-        description text,
-        value_labels text
+    locations.append(
+        Location(id=id, geometry=geometry, metadata=metadata)
     )
-''')
-dimension_id_and_name_pairs = []
 
+# %% dimensions
 time_size = dataset.dimensions['time'].size
-db_cursor.execute('''
-        insert into dimension (id, name, size, description, value_labels) values (?, ?, ?, ?, ?)
-    ''', [0, 'time', time_size, None, None])
-dimension_id_and_name_pairs.append([0, 'time'])
-
 layer_size = len(layers)
-db_cursor.execute('''
-        insert into dimension (id, name, size, description, value_labels) values (?, ?, ?, ?, ?)
-    ''', [1, 'layer', layer_size, None, None])
-dimension_id_and_name_pairs.append([1, 'layer'])
-# endregion
+dimension_time = Dimension(id=0, name='time', size=time_size)
+dimension_layer = Dimension(id=1, name='layer', size=layer_size,
+                            description=None, value_labels=['1', '2', '3'])
+dimensions = [
+    dimension_time,
+    dimension_layer
+]
 
-# region create variable and variable_dimension table
-db_cursor.execute('''
-    create table variable (
-        id integer primary key,
-        name varchar not null,
-        unit varchar,
-        description text
+# %% variables
+variables: Sequence[Variable] = []
+variable_and_nc_file_path_dict = {}
+for i in range(len(nc_file_path_and_variable_name_pairs)):
+    [nc_file_path, variable_name, description, unit,
+        has_layers] = nc_file_path_and_variable_name_pairs[i]
+    dimensions_for_the_variable = [dimension_time]
+    if has_layers:
+        dimensions_for_the_variable.append(dimension_layer)
+    variable = Variable(
+        id=i,
+        name=variable_name,
+        dimensions=dimensions_for_the_variable,
+        unit=unit,
+        description=description
     )
-''')
-db_cursor.execute('''
-    create table variable_dimension (
-        variable integer not null,
-        dimension integer not null,
-        foreign key (variable) references variable (id),
-        foreign key (dimension) references dimension (id),
-        primary key (variable, dimension)
-    )
-''')
-variable_id_and_detail_pairs = []
-index = 0
-for [nc_file_path, variable_name, description, unit, layers] in nc_file_path_and_variable_name_pairs:
-    db_cursor.execute('''
-        insert into variable (id, name, unit, description) values (?, ?, ?, ?)
-    ''', [index, variable_name, unit, description])
-    db_cursor.execute('''
-        insert into variable_dimension (variable, dimension) values (?, ?)
-    ''', [index, 0])
-    if layers:
-        db_cursor.execute('''
-            insert into variable_dimension (variable, dimension) values (?, ?)
-        ''', [index, 1])
-    variable_id_and_detail_pairs.append([index, [nc_file_path, variable_name, description, unit, layers]])
-    index += 1
-# endregion
+    variables.append(variable)
+    variable_and_nc_file_path_dict[variable] = nc_file_path
 
-# region create value table
-new_line_character = '\n'
-dimension_column_names = list(
-    map(lambda x: f'dimension_{x[0]}', dimension_id_and_name_pairs))
-db_cursor.execute(f'''
-    create table value (
-        location integer not null,
-        variable integar not null,
-        {f' integar,{new_line_character}'.join(dimension_column_names)},
-        value float,
-        foreign key (variable) references variable (id),
-        primary key (location, variable, {f', '.join(dimension_column_names)})
-    )
-''')
+# %% values
+values: Sequence[Value] = []
 
-for [variable_id, [nc_file_path, variable_name, description, unit, layers]] in variable_id_and_detail_pairs:
-    if layers:
+
+def set_value(locations, variable, layer=None):
+    current_variable = dataset.variables[variable.name]
+    for time in range(dimension_time.size):
+        for location in locations:
+            location_index = ids.index(location.id)
+            dimension_dict = {dimension_time: time}
+            if layer is not None:
+                dimension_dict[dimension_layer] = layer
+            values.append(Value(
+                location=location,
+                variable=variable,
+                value=float(current_variable[time][location_index][0]),
+                dimension_dict=dimension_dict
+            ))
+
+
+for variable in variables:
+    nc_file_path = variable_and_nc_file_path_dict[variable]
+    if len(list(filter(lambda d: d.name == 'layer', variable.dimensions))) > 0:
         for layer in layers:
             dataset = nc.Dataset(nc_file_path.replace('$$layer$$', str(layer)))
-            current_variable = dataset.variables[variable_name]
-            for time in range(dataset.dimensions['time'].size):
-                for location_id in location_ids:
-                    location_index = ids.index(location_id)
-                    value = float(current_variable[time][location_index][0])
-                    db_cursor.execute(f'''
-                        insert into value (location, variable, dimension_0, dimension_1, value) values (?, ?, ?, ?, ?)
-                    ''', [location_id, variable_id, time, layer - 1, value])
+            set_value(locations, variable, layer=layer - 1)
     else:
         dataset = nc.Dataset(nc_file_path)
-        current_variable = dataset.variables[variable_name]
-        # for only one dimension (time)
-        for time in range(dataset.dimensions['time'].size):
-            for location_id in location_ids:
-                location_index = ids.index(location_id)
-                value = float(current_variable[time][location_index][0])
-                db_cursor.execute(f'''
-                    insert into value (location, variable, dimension_0, dimension_1, value) values (?, ?, ?, ?, ?)
-                ''', [location_id, variable_id, time, None, value])
-# endregion
+        set_value(locations, variable)
 
-db_connection.commit()
+# %% main function
+generate_gwfvis_db(db_path, Options(
+    info=info,
+    locations=locations,
+    dimensions=dimensions,
+    variables=variables,
+    values=values
+))
+
+# %% finished
